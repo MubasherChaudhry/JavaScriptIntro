951c7c41260fc0b1fd047291d4382a45f8ba7e0c
Mubasher <mobi_20_11@yahoo.com>
2023-03-01 22:31:50 +0100

    first lesson

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Test");
+// always use console.log after attaching .js file in HTML.
+//go to live server with the help of HTML file
+// right click on browser and click on inspect and then click on console on appeared window in bottom.
+
+// Basic what we can do in javaScript
+// console.log() we can use to print in console
+// we can print any message within 'message' or "message" or `message`
+console.log("Hello world"); // Hello World
+// we can use mathematical operations and console.log will print the result in console
+// because javaScript is intelligent enough to understand these operations and solve them
+console.log(2 + 2); // 4
+console.log(10 - 2); // 8
+console.log(10 / 2); // 5
+console.log(10 * 2); // 20
+console.log(10 ** 2); // 100
+
+// so here is some basic introduction about js.
+// will see you soon in next lecture.
